5f6a5441c8aa7bbfd2b15cb3fd70519a6e26900d
John Furtado <jf8846@gmail.com>
2018-09-19 20:04:32 -0400

    driver class processes JSON

---
+++ src/main/java/com/luv2code/jackson/json/demo/Driver.java
@@ -0,0 +1,32 @@
+package com.luv2code.jackson.json.demo;
+
+import java.io.File;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		
+		try {
+			
+			// object mapper
+			ObjectMapper mapper = new ObjectMapper();
+			
+			// read JSON file and map to Java POJO: data/sample-lite.json
+			Student theStudent =
+					mapper.readValue(
+							new File("data/sample-lite.json"), Student.class)
+;			
+			System.out.println("Name: " + 
+								theStudent.getFirstName() + " " +
+								theStudent.getLastName());
+			
+		}
+		catch(Exception exception) {
+			exception.printStackTrace();
+		}
+		
+	}
+
+}
